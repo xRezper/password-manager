1cd7adee3feb76a6e1d14891a6f1cb32cb045f08
xRezper <xrezper210409@gmail.com>
2025-05-08 20:09:16 +0200

    made Dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,40 @@
 import customtkinter as ctk
+from PIL import Image
 
 
 def open_dashboard(app):
     dashboard = ctk.CTkToplevel()
-    dashboard.title("Dashboard")
+    dashboard.title("Password Manager")
     dashboard.geometry("800x600")
 
-    label = ctk.CTkLabel(dashboard, text="Dashboard")
+    label = ctk.CTkLabel(dashboard, text="")
     label.pack(pady=40)
 
-    logout_button = ctk.CTkButton(dashboard, text="Logout", command=lambda: [dashboard.destroy(), app.deiconify()])
+# Frame start
+
+    frame = ctk.CTkFrame(master=dashboard) 
+    frame.place(x=0, y=0, relwidth=0.2, relheight=1) 
+  
+    logo_image = ctk.CTkImage(Image.open('images/passwortgen.png'), size=(100, 100))
+        
+    logo_label = ctk.CTkLabel(master=frame, text="", image=logo_image)
+    logo_label.pack(pady=12, padx=10)
+
+
+    logout_button = ctk.CTkButton(frame, text="Logout", command=lambda: [dashboard.destroy(), app.deiconify()])
     logout_button.place(relx=0, rely=1, x=10, y=-10, anchor="sw")
+
     
+    start_button = ctk.CTkButton(frame, text="Startseite" )
+    start_button.pack(pady=12, padx=12, fill="x")
+
+    add_button = ctk.CTkButton(frame, text="Neuer Entry")
+    add_button.pack(pady=12, padx=12, fill="x")
+
+    view_button = ctk.CTkButton(frame, text="Übersicht")
+    view_button.pack(pady=12, padx=12, fill="x")
+    
+
     
 
     return dashboard
